4740ddef62e332fedb8e510ea55b1f8b377b9aa6
Raghuvar Tandon <raghuvartandon15@gmail.com>
2024-07-11 20:52:04 +0530

    deployed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/your-repo-name/', // Replace 'your-repo-name' with your GitHub repository name
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Adjust according to your dependencies
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600, // Adjust the chunk size warning limit if needed
+  },
+});
